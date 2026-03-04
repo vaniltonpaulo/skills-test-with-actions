6d708ec669d3197d51c54234672bb54c24c3d198
Vanilton paulo <120561451+vaniltonpaulo@users.noreply.github.com>
2026-03-04 01:24:37 +0000

    done

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -34,6 +34,16 @@ def test_area_of_circle_zero_radius():
     assert result == 0
 
 
+def test_area_of_circle_negative_radius():
+    """Test with a negative radius."""
+    # Arrange
+    radius = -1
+
+    # Act & Assert
+    with pytest.raises(ValueError):
+        area_of_circle(radius)
+
+
 def test_get_nth_fibonacci_zero():
     """Test with n=0."""
     # Arrange
@@ -59,12 +69,22 @@ def test_get_nth_fibonacci_one():
 
 
 def test_get_nth_fibonacci_ten():
- """Test with n=10."""
- # Arrange
- n = 10
+    """Test with n=10."""
+    # Arrange
+    n = 10
 
- # Act
- result = get_nth_fibonacci(n)
+    # Act
+    result = get_nth_fibonacci(n)
 
- # Assert
- assert result == 55
+    # Assert
+    assert result == 55
+
+
+def test_get_nth_fibonacci_negative():
+    """Test with negative n."""
+    # Arrange
+    n = -1
+
+    # Act & Assert
+    with pytest.raises(ValueError):
+        get_nth_fibonacci(n)
